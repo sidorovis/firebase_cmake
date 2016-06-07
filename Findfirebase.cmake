8922f509273852c65cdb3d52ef19bafe49779b06
Ivan Sidarau <ivan.sidarau@viber.com>
2016-06-07 18:44:30 +0300

    added implementation for find mechanisms

--- Findfirebase.cmake
+++ Findfirebase.cmake
@@ -14,7 +14,12 @@
 # 2. firebase_INCLUDE_DIRS (list of folders where firebase sdk headers are situated);
 # 3. firebase_LIBRARIES (list of paths to the installed libraries).
 #
+# Supported next components:
+# 1. app
+# 2. analytics
+#
 
+# root folder definition
 
 if (firebase_ROOT)
     set(firebase_root ${firebase_ROOT})
@@ -32,6 +37,84 @@ else()
 endif()
 
 if (firebase_FIND_DEBUG)
-    message(STATUS "We will look for firebase sdk at ${firebase_root} path")
+    message(STATUS "[DEBUG]: We will look for firebase sdk at ${firebase_root} path")
 endif()
+
+set(firebase_FOUND FALSE)
+set(firebase_INCLUDE_DIRS "")
+set(firebase_LIBRARIES "")
+
+# component list definition
+
+set(components app analytics)
+
+if (NOT ${firebase_FIND_COMPONENTS})
+    if (firebase_FIND_DEBUG)
+	message(STATUS "[DEBUG]: We will look for all possible components (${components})")
+    endif()
+    set(firebase_FIND_COMPONENTS ${components})
+endif()
+
+set(firebase_LIBRARIES_FOLDER folder-NOTFOUND)
+
+if(UNIX AND NOT APPLE AND NOT CYGWIN)
+    set(firebase_LIBRARIES_FOLDER "${firebase_root}/libs/linux")
+else()
+    message(FATAL_ERROR "Not supported (yet) architecture / operation system / compiler")
+endif()
+
+if (firebase_FIND_DEBUG)
+    message(STATUS "[DEBUG]: Path that we will look for libraries is ${firebase_LIBRARIES_FOLDER}")
+endif()
+
+# component dependency resolve
+
+list(FIND firebase_FIND_COMPONENTS app firebase_app_found)
+list(FIND firebase_FIND_COMPONENTS analytics firebase_analytics_found)
+if ((("${firebase_analytics_found}" GREATER "-1")) AND (NOT ("${firebase_app_found}" GREATER "-1")))
+    if (firebase_FIND_DEBUG)
+        message(STATUS "[DEBUG]: Component 'analytics' found into {find component list}, so 'app' should also get to the list.")
+    endif()
+    set(firebase_FIND_COMPONENTS app ${firebase_FIND_COMPONENTS})
+    list(FIND firebase_FIND_COMPONENTS app firebase_app_found)
+endif()
+
+if (firebase_FIND_DEBUG)
+    message(STATUS "[DEBUG]: Actual components list (after dependencies resolve): ${firebase_FIND_COMPONENTS}")
+endif()
+
+# path search
+
+foreach(component ${firebase_FIND_COMPONENTS})
+    list(FIND components "${component}" we_should_find_${component})
+    if (firebase_FIND_DEBUG)
+	message(STATUS "[DEBUG]: We are looking for firebase '${component}' component and index into existing components list is ${we_should_find_${component}}")
+    endif()
+    if (NOT "${we_should_find_${component}}" GREATER "-1")
+	if (firebase_FIND_REQUIRED)
+	    message(FATAL_ERROR "Unknown firebase component '${component}' was added to find_package command")
+	elseif(firebase_FIND_DEBUG)
+	    message(WARNING "[DEBUG]: ${component} component is not in a list of available components")
+	    continue()
+	endif()
+    endif()
+
+    set(firebase_${component}_FOUND FALSE)
+    set(firebase_${component}_INCLUDE_DIRS ${component}-NOTFOUND)
+    set(firebase_${component}_LIBRARIES ${component}-NOTFOUND)
+
+    find_path(firebase_${component}_INCLUDE_DIRS
+		NAMES "${component}.h"
+		HINTS "${firebase_root}/include/firebase")
+
+    find_library(firebase_${component}_LIBRARY
+		NAMES "${component}"
+		HINTS "${firebase_LIBRARIES_FOLDER}")
+
+    if (NOT firebase_FIND_QUIETLY)
+	message(STATUS "Firebase ${component} header path: '${firebase_${component}_INCLUDE_DIRS}'")
+	message(STATUS "Firebase ${component} libraries: '${firebase_${component}_LIBRARY}'")
+    endif()
+
+endforeach()
 
--- README.md
+++ README.md
@@ -1,11 +1,33 @@
-# firebase_cmake
-CMake Finder module for Firebase
+firebase_cmake
+--------------
 
-This is an CMake module that helps to locate Firebase for your platform using find_package.
+CMake Finder module for Firebase (http://firebase.google.com/)
+
+# Requirements:.
+
+1. firebase_ROOT (FIREBASE_ROOT also acceptable) for example: set(firebase_ROOT ~/firebase_cpp_sdk);
+OR.
+2. environment variable FIREBASE_ROOT should exists.
+
+
+# Optional variables:
+
+1. firebase_FIND_DEBUG - will generate much bigger amount of messages and will help to debug find firebase process.
+
+
+# Output variables and search paths are:
+
+1. firebase_FOUND (FALSE, TRUE);
+2. firebase_INCLUDE_DIRS (list of folders where firebase sdk headers are situated);
+3. firebase_LIBRARIES (list of paths to the installed libraries).
+
+
+# Supported components:
+
+1. app
+2. analytics
 
 Usage examples from your CMake code:
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/firebase_cmake")
-
-find_package(Firebase)
-
+find_package(firebase QUIET REQUIRED COMPONENTS app analytics)
 target_link_libraries(${project_name} firebase)
